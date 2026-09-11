7409051ef0e9584185f1dd0120db608eec64fedc
Alvaro Colunga <alvaro.colunga@gmail.com>
2026-09-11 16:23:22 -0500

    Se agrego vista y controlador para Login

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,12 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using HelpDeskWeb.Models;
+using HelpDeskWeb.ViewModels;
 
 namespace HelpDeskWeb.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        private readonly SignInManager<User> signInManager;
+        private readonly UserManager<User> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AccountController(SignInManager<User> signInManager, UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+
+            if(result.Succeeded)
+            {
+                return RedirectToAction("Index","Home");
+            }
+
+            ModelState.AddModelError(String.Empty, "Error al iniciar sesión");
+
+            return View(model);
+        }
+
     }
 }
